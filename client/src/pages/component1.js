29f628b31f737f01c25dd1ec57a4a07faaa25dd1
hirudaka <hirudakakumarasingha@gmail.com>
2024-04-06 21:51:49 +0530

    frontend setup done

---
+++ client/src/pages/component1.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function Component1() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+        <p>
+          Edit src/App.js ba halo and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"> Hallo react kohomada </a>
+      </header>
+    </div>
+  );
+}
+
+export default Component1;
